286bc915517d6bfcd699d29fe9fd0c02207e0351
kennedybm <kennedy_muniz@hotmail.com>
2023-04-27 03:19:35 -0300

    feat: add usersDetailsService to the serives main exports

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,3 +1,4 @@
 import { listAllUsersService } from "./users/listAllUsers.service";
+import { usersDetailsService } from "./users/usersDetails.service";
 
-export { listAllUsersService };
+export { listAllUsersService, usersDetailsService };
