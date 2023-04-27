5f97267c8abedb8f218aa03272191f4b272fba06
kennedybm <kennedy_muniz@hotmail.com>
2023-04-27 01:49:07 -0300

    feat: created app and add users route to Express, the route is mounted  at /api/users and uses  the usersRoute router. Additionally the app is configured to use JSON, middleware and CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,9 +3,16 @@ import "express-async-errors";
 import "reflect-metadata";
 import cors from "cors";
 
+import handleAppErrorMiddleware from "./middlewares/errors/handleAppError.middleware";
+import usersRoute from "./routers/users/users.routes";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/users", usersRoute);
+
+app.use(handleAppErrorMiddleware);
 
 export default app;
