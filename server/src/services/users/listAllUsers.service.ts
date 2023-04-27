7c1c460c4d348ac0eaf625b387ae419fc90b0e62
kennedybm <kennedy_muniz@hotmail.com>
2023-04-27 03:16:51 -0300

    fix: parsedLinks, response variables and return moved inside to teh try escope

--- server/src/services/users/listAllUsers.service.ts
+++ server/src/services/users/listAllUsers.service.ts
@@ -3,8 +3,6 @@ import { AppError } from "../../errors/AppError";
 
 export const listAllUsersService = async (userId: string) => {
   const parsedId = parseInt(userId);
-  let parsedLinks: string[] | undefined = [];
-  let response: Object = {};
 
   try {
     const retrievedData = await octokit.request("GET /users{?since}", {
@@ -14,16 +12,16 @@ export const listAllUsersService = async (userId: string) => {
       },
     });
 
-    parsedLinks = retrievedData.headers.link?.split(",");
+    let parsedLinks: string[] | undefined = retrievedData.headers.link?.split(",");
 
-    response = {
+    let response: Object = {
       data: retrievedData.data,
       firstPage: parsedLinks![1],
       nextPage: parsedLinks![0],
     };
+
+    return response;
   } catch (error: unknown) {
     throw new AppError(304, "Not modified");
   }
-
-  return response;
 };
