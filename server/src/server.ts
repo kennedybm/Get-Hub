99a3f37fc8b159db195917183a34a8d3d085f7f4
kennedybm <kennedy_muniz@hotmail.com>
2023-04-27 01:42:41 -0300

    feat: creted server.ts file, wich is responsible to start the server using the listen method from the app module. The server will listen to the port specified in PORT environment variable or to port 5500 as a default. A message is also logged to the console indicating the port number that the server is running on. The dotenv module is used to load environment variables from a .env file.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,6 @@
 import "dotenv/config";
-import mongoose from "mongoose";
 import app from "./app";
 
-mongoose
-  .connect(process.env.MONGO_URL!)
-  .then(() => {
-    app.listen(process.env.PORT || 5500, () =>
-      console.log(`Running on port ${process.env.PORT || 5500}`)
-    );
-  })
-  .catch((err) => {
-    console.log(400, `${err.message}`);
-  });
+app.listen(process.env.PORT || 5500, () =>
+  console.log(`Running on port ${process.env.PORT || 5500}`)
+);
