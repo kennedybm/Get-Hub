b84080e95e19ceb5a334a99aaec881ed6a36a7f6
kennedybm <kennedy_muniz@hotmail.com>
2023-04-27 11:50:41 -0300

    feat: add usersRepositoriesService to the main services exports

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,4 +1,5 @@
 import { listAllUsersService } from "./users/listAllUsers.service";
 import { usersDetailsService } from "./users/usersDetails.service";
+import { usersRepositoriesService } from "./users/usersRepositories.service";
 
-export { listAllUsersService, usersDetailsService };
+export { listAllUsersService, usersDetailsService, usersRepositoriesService };
