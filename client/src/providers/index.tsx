db9b45fec5d4f77029900424ee1a36824dd4a221
kennedybm <kennedy_muniz@hotmail.com>
2023-04-27 14:02:36 -0300

    feat: created Providers component, which takes a children prop of type ReactNode and wraps it in a fragment. This component will be used to wrap the upcoming providers components in order to provide context.

---
+++ client/src/providers/index.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+interface IProvidersProps {
+  children: ReactNode;
+}
+
+const Providers = ({ children }: IProvidersProps) => {
+  return <>{children}</>;
+};
+export default Providers;
