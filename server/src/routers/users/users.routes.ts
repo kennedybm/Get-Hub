076b05c7de907aa3b21e47de13868b968dc7fbd3
kennedybm <kennedy_muniz@hotmail.com>
2023-04-27 03:13:35 -0300

    feat: add a new endpoint for retrieving users details to the usersRoute. The endpoint is located at /:username/details and uses the usersDetailsController function as the controller for handling requests

--- server/src/routers/users/users.routes.ts
+++ server/src/routers/users/users.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { listAllUsersController } from "../../controllers";
+import { listAllUsersController, usersDetailsController } from "../../controllers";
 
 const usersRoute = Router();
 
 //listAll
 usersRoute.get("/", listAllUsersController);
+
+//listDetails
+usersRoute.get("/:username/details", usersDetailsController);
 
 export default usersRoute;
