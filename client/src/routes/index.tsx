cea0b821514f659c5d37f7ccd6cb6bf681a2b82b
kennedybm <kennedy_muniz@hotmail.com>
2023-04-28 02:38:21 -0300

    feat: add route for user details, defined a new 'Route' component for the 'details:username' path, with the 'Details' component as the component prop. This change enables users to navigate and see the details of a specific use.

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../pages/home";
+import Details from "../pages/details";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/details/:userName" component={Details} />
       </Switch>
     </Router>
   );
