ac8dfec20c8b6d9cfc367dcd876eff02cdf502d4
kennedybm <kennedy_muniz@hotmail.com>
2023-04-28 02:22:41 -0300

    feat: add exports for 'Pagination', and 'DetailsCard' components to allow them to be used in other parts of the application.

---
+++ client/src/components/index.ts
@@ -0,0 +1,6 @@
+import { Users } from "./user";
+import { UserCard } from "./userCards";
+import { Pagination } from "./pagination";
+import { DetailsCard } from "./detailsCard";
+
+export { Users, UserCard, Pagination, DetailsCard };
