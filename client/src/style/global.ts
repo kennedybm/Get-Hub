a46348704613094570ef088e1bee42238bb7b0c5
kennedybm <kennedy_muniz@hotmail.com>
2023-04-28 02:39:15 -0300

    feat: add set up for the project font sizes.

--- client/src/style/global.ts
+++ client/src/style/global.ts
@@ -11,13 +11,24 @@ export default createGlobalStyle`
         font-size: 100%;
     }
 :root{
-    --default: #f8f8f8;
-    --decor: #ffc900;
+    --default: #0d1017;
+    --decor: #161a22;
 
-    --gray0: #f3f3f3;
-    --gray1: #6f5b61;
-    --gray2: #222222;
-    --gray3: #e9e9e9;
+    --details: #33373f;
+    --details1: #686868;
+    --font: #a9afb9;
+    --title: #3c577a;
+
+    --text: 'Montserrat', sans-serif;
+    --content: 'Merriweather', serif;
+
+    --fs-tt: clamp(1rem, 7vw, 2.3rem);
+    --fs-xl: clamp(1.9rem, 6vw, 2.5rem);
+    --fs-xm: 1.9rem;
+    --fs-lrg: 1.8rem;
+    --fs-mid: 1.2rem;
+    --fs-sml: 0.9rem;
+    --fs-xs: 0.6rem;
 }
 body{
     background-color: var(--default);
